a5d2d39154488181efb848f5e0c1d83a5df2363c
John Ottenlips <john@jammber.com>
2016-12-14 10:14:54 -0600

    Initial Commit

---
+++ MusicBrainziPhone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicBrainziPhone
+//
+//  Created by John Ottenlips on 12/14/16.
+//  Copyright © 2016 jottenlips. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
