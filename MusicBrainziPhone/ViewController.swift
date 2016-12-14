388486f611ec49ef0c9221ddb5ca731f70e82418
John Ottenlips <john@jammber.com>
2016-12-14 12:10:15 -0600

    podfile

--- MusicBrainziPhone/ViewController.swift
+++ MusicBrainziPhone/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func pressedSearchButton(_ sender: AnyObject) {
+        print("button pressed")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
--- Podfile
+++ Podfile
@@ -1,4 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
 target 'MusicBrainziPhone' do
-	use_frameworks!
-	pod 'Alamofire', '~> 3.0'
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+	
+  use_frameworks!
+
+  # Pods for MusicBrainziPhone
+  pod 'Alamofire', '~> 4.0'
+
 end
