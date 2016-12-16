56cce0031b3b257139fe119cd86eb54cb468892e
John Ottenlips <jottenlips@gmail.com>
2016-12-16 12:38:41 -0600

    moved data function to uth file

--- Auth.swift
+++ Auth.swift
@@ -8,3 +8,28 @@
 
 import Foundation
 import NXOAuth2Client
+import SwiftyJSON
+import Alamofire
+
+class Auth{
+    
+    func getData(searchTerm:String, searchType:String){
+        let query = searchTerm.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        Alamofire.request("https://musicbrainz.org/ws/2/"+searchType+"/?fmt=json&query="+query!).responseJSON { response in
+            print(response.request)  // original URL request
+            print(response.response) // HTTP URL response
+            print(response.data)     // server data
+            print(response.result)   // result of response serialization
+            //            if let JSON = response.result.value {
+            //                print("JSON: \(JSON)")
+            //            }
+            if let result = response.result.value {
+                //                let JSON = result
+                let data = JSON(result)
+                //                let list = JSON["artist"] as! NSArray
+                print(data["artists"][0])
+            }
+        }
+    }
+
+}
--- MusicBrainziPhone/ViewController.swift
+++ MusicBrainziPhone/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
     @IBAction func pressedSearchButton(_ sender: AnyObject) {
         print("button pressed")
         if let theSearchTerm = self.searchField.text{
-        getData(searchTerm: theSearchTerm, searchType: "artist")
+        Auth().getData(searchTerm: theSearchTerm, searchType: "artist")
         }
         print(self.searchField.text!)
     }
@@ -30,25 +30,7 @@ class ViewController: UIViewController {
     let BASE_URL = "http://musicbrainz.org/ws/2/"
     let format_query = "?fmt=json&query="
     
-    func getData(searchTerm:String, searchType:String){
-        let query = searchTerm.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
-        Alamofire.request("https://musicbrainz.org/ws/2/"+searchType+"/?fmt=json&query="+query!).responseJSON { response in
-            print(response.request)  // original URL request
-            print(response.response) // HTTP URL response
-            print(response.data)     // server data
-            print(response.result)   // result of response serialization
-//            if let JSON = response.result.value {
-//                print("JSON: \(JSON)")
-//            }
-            if let result = response.result.value {
-//                let JSON = result
-                let data = JSON(result)
-//                let list = JSON["artist"] as! NSArray
-                print(data["artists"][0])
-            }
-        }
-    }
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
