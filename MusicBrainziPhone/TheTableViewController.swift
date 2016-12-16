51ea2c02a229b7b046397013208f08f4769107ed
John Ottenlips <jottenlips@gmail.com>
2016-12-16 14:36:21 -0600

    second view controller

--- MusicBrainziPhone/TheTableViewController.swift
+++ MusicBrainziPhone/TheTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TheTableViewController: ViewController {
+class TheTableViewController: UITableViewController {
 
     
     override func viewDidLoad() {
@@ -28,18 +28,18 @@ class TheTableViewController: ViewController {
 
     // MARK: - Table view data source
 
-    func numberOfSections(in tableView: UITableView) -> Int {
+    override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0
     }
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
 
    
-     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
         // Configure the cell...
